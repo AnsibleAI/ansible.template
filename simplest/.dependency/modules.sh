ee0028db61bba763537eb118cd9f1cad8ae6aaeb
Dark Star☀🎵 <darkstar@unisocial.net>
2023-04-18 17:38:26 -0400

    update

---
+++ simplest/.dependency/modules.sh
@@ -0,0 +1,17 @@
+url="https://github.com/Unicorn-OS/Snapcraft.git"
+package="Snapcraft"
+role="Snapcraft"
+
+
+link_dependency(){
+  dir=".dependency"
+  ansible_dir="ansible/roles/dependency"
+  cd ..
+  ln -srf $dir/$package/ansible/roles/$role ansible/roles/.dependency/
+}
+
+main(){
+  link_dependency
+}
+
+main
