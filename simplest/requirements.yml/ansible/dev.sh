fd98804587fdc62a009956272f1210a9a43d8dd5
UniSocial <uni@ultura.net>
2024-10-07 17:34:35 -0400

    try

--- simplest/requirements.yml/ansible/dev.sh
+++ simplest/requirements.yml/ansible/dev.sh
@@ -1,2 +1,4 @@
 cd roles
-git clone git@github.com:Metal-AI/ansible-role-MAAS-config.git
+
+git clone git@github.com:Unicorn-OS/ansible-role-AptCacherNg-Client.git
+ln -srf ansible-role-AptCacherNg-Client depends/UniInc.AptCacherNg-Client
